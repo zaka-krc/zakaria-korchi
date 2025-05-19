38828ac4eec8bf668428e00c699b27ff421da5a5
zaka-krc <zakariakorchi2003@hotmail.com>
2025-05-19 13:31:13 +0200

    initial commit

---
+++ src/main/java/be/ehb/zakaria_korchi/ZakariaKorchiApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.zakaria_korchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakariaKorchiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZakariaKorchiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/zakaria_korchi/ZakariaKorchiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.zakaria_korchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZakariaKorchiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
